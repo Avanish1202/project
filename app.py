1a610ed5e56ba85fe3021b555ea7da197ee7095a
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 20:42:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
 import pickle
+import gzip
 import streamlit as st
+import requests
 import io
 
-# Function to fetch data from a local file
-def fetch_data_from_file(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            return file.read()
-    except Exception as e:
-        st.error(f"Failed to load data from file: {file_path}\nError: {e}")
+# Function to fetch data from a URL
+def fetch_data_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        st.error(f"Failed to load data from URL: {url}")
         return None
 
-# Specify the file paths for movie data and similarity data
-movie_data_path = 'movie_list.pkl'
-similarity_data_path = 'similarity.pkl'
+# Specify the URLs for movie data and similarity data
+movie_data_url = 'movie_list.pkl'
+similarity_data_url = 'similarity.pkl.gz'
 
 # Load movie data
-movie_data = fetch_data_from_file(movie_data_path)
+movie_data = fetch_data_from_url(movie_data_url)
 if movie_data:
     try:
         movies = pickle.loads(movie_data)
     except Exception as e:
-        st.error(f"Failed to load movie data from file: {movie_data_path}\nError: {e}")
-        movies = None
+        st.error(f"Failed to load movie data from URL: {movie_data_url}\nError: {e}")
+        st.stop()
 
-# Load similarity data
-similarity_data = fetch_data_from_file(similarity_data_path)
+# Load compressed similarity data from another repository
+similarity_data = fetch_data_from_url(similarity_data_url)
 if similarity_data:
     try:
-        similarity = pickle.loads(similarity_data)
+        with gzip.decompress(similarity_data) as f:
+            similarity = pickle.load(f)
     except Exception as e:
-        st.error(f"Failed to load similarity data from file: {similarity_data_path}\nError: {e}")
-        similarity = None
+        st.error(f"Failed to load compressed similarity data from URL: {similarity_data_url}\nError: {e}")
+        st.stop()
 
 # Function to recommend movies based on similarity
 def recommend(selected_movie):
@@ -40,15 +43,27 @@ def recommend(selected_movie):
     if not selected_movie_index.empty:
         index = selected_movie_index[0]
 
-        movie_similarity_scores = similarity[index]
-        distances = sorted(enumerate(movie_similarity_scores), reverse=True, key=lambda x: x[1])
+        # Get similarity scores for the selected movie
+        try:
+            movie_similarity_scores = similarity[index]
+        except IndexError:
+            st.error(f"IndexError: Index {index} is out of bounds for the 'similarity' array.")
+            return None
 
+        # Sort movies based on similarity scores
+        distances = sorted(list(enumerate(movie_similarity_scores)), reverse=True, key=lambda x: x[1])
+
+        # Get top 5 recommendations (excluding the selected movie itself)
         top_recommendations = []
-        for i in range(1, 6):  # Start from 1 to exclude the selected movie
-            recommended_index = distances[i][0]
-            recommended_movie_name = movies.iloc[recommended_index]['title']
-            recommended_movie_poster = movies.iloc[recommended_index]['poster_path']
-            top_recommendations.append((recommended_movie_name, recommended_movie_poster))
+        for i in range(1, min(6, len(distances))):  # Start from 1 to exclude the selected movie
+            try:
+                recommended_index = distances[i][0]
+                recommended_movie_name = movies.iloc[recommended_index]['title']
+                recommended_movie_poster = movies.iloc[recommended_index]['poster_path']
+                top_recommendations.append((recommended_movie_name, recommended_movie_poster))
+            except IndexError:
+                st.error(f"IndexError: Index {recommended_index} is out of bounds for the 'movies' array.")
+                return None
 
         return top_recommendations
     else:
