9185673debaceabd68870e91f6ece290b5b68b26
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 23:23:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def fetch_data_from_file(file_path):
 
 # Specify the file paths for movie data and similarity data
 movie_data_path = 'movie_list.pkl'
-similarity_data_path = 'similarity.pkl'
+similarity_data_path = 'https://www.dropbox.com/home?preview=similarity.pkl'
 
 # Load movie data
 movie_data = fetch_data_from_file(movie_data_path)
