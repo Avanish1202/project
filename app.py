7863eac8c94d5caea48ff98fd2bd8566bba8873b
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 20:30:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,91 +1,63 @@
 import pickle
 import streamlit as st
-import io
-
-# Function to fetch data from a local file
-def fetch_data_from_file(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            return file.read()
-    except Exception as e:
-        st.error(f"Failed to load data from file: {file_path}\nError: {e}")
-        return None
-
-# Specify the file paths for movie data and similarity data
-movie_data_path = 'movie_list.pkl'
-similarity_data_path = 'similarity.pkl'
-
-# Load movie data
-movie_data = fetch_data_from_file(movie_data_path)
-if movie_data:
-    try:
-        movies = pickle.loads(movie_data)
-    except Exception as e:
-        st.error(f"Failed to load movie data from file: {movie_data_path}\nError: {e}")
-        st.stop()
-
-# Load similarity data
-similarity_data = fetch_data_from_file(similarity_data_path)
-if similarity_data:
-    try:
-        similarity = pickle.loads(similarity_data)
-    except Exception as e:
-        st.error(f"Failed to load similarity data from file: {similarity_data_path}\nError: {e}")
-        st.stop()
-
-# Function to recommend movies based on similarity
-# Function to recommend movies based on similarity
-def recommend(selected_movie):
-    selected_movie_index = movies[movies['title'] == selected_movie].index
-
-    if not selected_movie_index.empty:
-        index = selected_movie_index[0]
-
-        print(f"Selected Movie Index: {index}")  # Add this line
-
-        # Get similarity scores for the selected movie
-        try:
-            movie_similarity_scores = similarity[index]
-            print(f"Similarity Scores: {movie_similarity_scores}")  # Add this line
-        except IndexError:
-            st.error(f"IndexError: Index {index} is out of bounds for the 'similarity' array.")
-            return None
-
-        # Sort movies based on similarity scores
-        distances = sorted(list(enumerate(movie_similarity_scores)), reverse=True, key=lambda x: x[1])
-
-        # Get top 5 recommendations (excluding the selected movie itself)
-        top_recommendations = []
-        for i in range(1, min(6, len(distances))):  # Start from 1 to exclude the selected movie
-            try:
-                recommended_index = distances[i][0]
-                recommended_movie_name = movies.iloc[recommended_index]['title']
-                recommended_movie_poster = movies.iloc[recommended_index]['poster_path']
-                top_recommendations.append((recommended_movie_name, recommended_movie_poster))
-            except IndexError:
-                st.error(f"IndexError: Index {recommended_index} is out of bounds for the 'movies' array.")
-                return None
-
-        return top_recommendations
-    else:
-        st.error(f"Selected movie '{selected_movie}' not found.")
-        return None
+import requests
 
 
-# Define the Streamlit app
-def main():
-    st.header('Movie Recommender System')
-    movie_list = movies['title'].values
-    selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+# Function to fetch the movie poster
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(
+        movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
-    if st.button('Show Recommendation'):
-        recommended_movies = recommend(selected_movie)
 
-        if recommended_movies:
-            for recommended_movie_name, recommended_movie_poster in recommended_movies:
-                st.text(recommended_movie_name)
-                st.image(recommended_movie_poster)
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-# Run the Streamlit app
-if __name__ == '__main__':
-    main()
+    return recommended_movie_names, recommended_movie_posters
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    # Create columns
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    # Add content to columns
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
