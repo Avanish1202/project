e5f9040ee1cf02699e01a58f3d0b01628f7c06be
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 20:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
 import pickle
-import gzip
 import streamlit as st
-import requests
 import io
 
-# Function to fetch data from a URL
-def fetch_data_from_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-    else:
-        st.error(f"Failed to load data from URL: {url}")
+# Function to fetch data from a local file
+def fetch_data_from_file(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            return file.read()
+    except Exception as e:
+        st.error(f"Failed to load data from file: {file_path}\nError: {e}")
         return None
 
-# Specify the URLs for movie data and similarity data
-movie_data_url = 'movie_list.pkl'
-similarity_data_url = 'similarity.pkl.gz'
+# Specify the file paths for movie data and similarity data
+movie_data_path = 'movie_list.pkl'
+similarity_data_path = 'similarity.pkl.gz'
 
 # Load movie data
-movie_data = fetch_data_from_url(movie_data_url)
+movie_data = fetch_data_from_file(movie_data_path)
 if movie_data:
     try:
         movies = pickle.loads(movie_data)
     except Exception as e:
-        st.error(f"Failed to load movie data from URL: {movie_data_url}\nError: {e}")
-        st.stop()
+        st.error(f"Failed to load movie data from file: {movie_data_path}\nError: {e}")
+        movies = None
 
-# Load compressed similarity data from another repository
-similarity_data = fetch_data_from_url(similarity_data_url)
+# Load similarity data
+similarity_data = fetch_data_from_file(similarity_data_path)
 if similarity_data:
     try:
-        with gzip.decompress(similarity_data) as f:
-            similarity = pickle.load(f)
+        similarity = pickle.loads(similarity_data)
     except Exception as e:
-        st.error(f"Failed to load compressed similarity data from URL: {similarity_data_url}\nError: {e}")
-        st.stop()
+        st.error(f"Failed to load similarity data from file: {similarity_data_path}\nError: {e}")
+        similarity = None
 
 # Function to recommend movies based on similarity
 def recommend(selected_movie):
