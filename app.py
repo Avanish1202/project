9b41a891054a9b3374cb6dbd475080f7324e1e52
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 21:26:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def fetch_data_from_file(file_path):
 
 # Specify the file paths for movie data and similarity data
 movie_data_path = 'movie_list.pkl'
-similarity_data_path = 'https://drive.google.com/uc?id=18IVlSYFX5CywLXfJP1gp-AtN_GPSsgZN'
+similarity_data_path = 'https://drive.google.com/drive/u/1/my-drive'
 
 # Load movie data
 movie_data = fetch_data_from_file(movie_data_path)
