97b58fc0a4897d6e384f6950de77e29574c9fc3b
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 23:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,29 @@
 import pickle
 import streamlit as st
+import requests
+import io
 
-# Function to fetch data from a local file
-def fetch_data_from_file(file_path):
+# Function to fetch data from a URL
+def fetch_data_from_url(url):
     try:
-        with open(file_path, 'rb') as file:
-            return file.read()
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.content
     except Exception as e:
-        st.error(f"Failed to load data from file: {file_path}\nError: {e}")
+        st.error(f"Failed to load data from URL: {url}\nError: {e}")
         return None
 
-# Specify the file paths for movie data and similarity data
-movie_data_path = 'movie_list.pkl'
-similarity_data_path = 'https://www.dropbox.com/scl/fi/vs3b5hk78j10wvnecduwx/similarity.pkl?rlkey=7g1j2iuuvdwtl37ohyu8jnthx&dl=0'
-
-# Load movie data
-movie_data = fetch_data_from_file(movie_data_path)
-if movie_data:
-    try:
-        movies = pickle.loads(movie_data)
-    except Exception as e:
-        st.error(f"Failed to load movie data from file: {movie_data_path}\nError: {e}")
-        st.stop()
+# Specify the URL for similarity data
+similarity_data_url = 'https://www.dropbox.com/scl/fi/vs3b5hk78j10wvnecduwx/similarity.pkl?rlkey=7g1j2iuuvdwtl37ohyu8jnthx&dl=0'
 
 # Load similarity data
-similarity_data = fetch_data_from_file(similarity_data_path)
+similarity_data = fetch_data_from_url(similarity_data_url)
 if similarity_data:
     try:
         similarity = pickle.loads(similarity_data)
     except Exception as e:
-        st.error(f"Failed to load similarity data from file: {similarity_data_path}\nError: {e}")
+        st.error(f"Failed to load similarity data from URL: {similarity_data_url}\nError: {e}")
         st.stop()
-
-# Rest of the code remains unchanged...
 
 
 # Function to recommend movies based on similarity
