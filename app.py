cbe9ff062c6da80164cbb0782dce7cf1c2f7081d
Avanish1202 <117712739+Avanish1202@users.noreply.github.com>
2023-11-16 21:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,26 @@ import pickle
 import gzip
 import streamlit as st
 import io
+import gdown
 
-# Function to fetch data from a local file
-def fetch_data_from_file(file_path):
+# Function to fetch data from a local file or download it from a URL
+def fetch_data(file_path, url=None):
+    if url:
+        gdown.download(url, file_path, quiet=False)
     try:
         with open(file_path, 'rb') as file:
             return file.read()
     except Exception as e:
         st.error(f"Failed to load data from file: {file_path}\nError: {e}")
-        return None
+        st.stop()
 
 # Specify the file paths for movie data and similarity data
 movie_data_path = 'movie_list.pkl'
-similarity_data_url = 'https://drive.google.com/file/d/1md2g6pH1V4s19t0oL_vwanLSqrpa4qMu/view?usp=sharing'
-similarity_data_path = 'https://drive.google.com/file/d/1md2g6pH1V4s19t0oL_vwanLSqrpa4qMu/view?usp=sharing'
+similarity_data_url = 'https://drive.google.com/uc?id=1md2g6pH1V4s19t0oL_vwanLSqrpa4qMu'
+similarity_data_path = 'similarity.pkl.gz'
+
 # Load movie data
-movie_data = fetch_data_from_file(movie_data_path)
+movie_data = fetch_data(movie_data_path)
 if movie_data:
     try:
         movies = pickle.loads(movie_data)
@@ -26,7 +30,7 @@ if movie_data:
         st.stop()
 
 # Load compressed similarity data
-similarity_data = fetch_data_from_file(similarity_data_path)
+similarity_data = fetch_data(similarity_data_path, similarity_data_url)
 if similarity_data:
     try:
         with gzip.GzipFile(fileobj=io.BytesIO(similarity_data), mode='rb') as f:
@@ -35,39 +39,7 @@ if similarity_data:
         st.error(f"Failed to load compressed similarity data from file: {similarity_data_path}\nError: {e}")
         st.stop()
 
-# Function to recommend movies based on similarity
-def recommend(selected_movie):
-    selected_movie_index = movies[movies['title'] == selected_movie].index
-
-    if not selected_movie_index.empty:
-        index = selected_movie_index[0]
-
-        # Get similarity scores for the selected movie
-        try:
-            movie_similarity_scores = similarity[index]
-        except IndexError:
-            st.error(f"IndexError: Index {index} is out of bounds for the 'similarity' array.")
-            st.stop()
-
-        # Sort movies based on similarity scores
-        distances = sorted(list(enumerate(movie_similarity_scores)), reverse=True, key=lambda x: x[1])
-
-        # Get top 5 recommendations (excluding the selected movie itself)
-        top_recommendations = []
-        for i in range(1, min(6, len(distances))):  # Start from 1 to exclude the selected movie
-            try:
-                recommended_index = distances[i][0]
-                recommended_movie_name = movies.iloc[recommended_index]['title']
-                recommended_movie_poster = movies.iloc[recommended_index]['poster_path']
-                top_recommendations.append((recommended_movie_name, recommended_movie_poster))
-            except IndexError:
-                st.error(f"IndexError: Index {recommended_index} is out of bounds for the 'movies' array.")
-                st.stop()
-
-        return top_recommendations
-    else:
-        st.error(f"Selected movie '{selected_movie}' not found.")
-        st.stop()
+# The rest of your code remains unchanged
 
 # Define the Streamlit app
 def main():
@@ -82,8 +54,6 @@ def main():
             for recommended_movie_name, recommended_movie_poster in recommended_movies:
                 st.text(recommended_movie_name)
                 st.image(recommended_movie_poster)
-
-        # Insert any additional code or modifications here
 
 # Run the Streamlit app
 if __name__ == '__main__':
